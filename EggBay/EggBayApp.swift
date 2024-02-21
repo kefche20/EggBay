ad3f2a08423efe45b3b11e80b1e7b8d8f2283e9a
Nedyalko Tenev <ntenev@outlook.com>
2024-02-21 12:39:37 +0100

    Initial Commit

---
+++ EggBay/EggBayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggBayApp.swift
+//  EggBay
+//
+//  Created by Nedyalko Tenev on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct EggBayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
