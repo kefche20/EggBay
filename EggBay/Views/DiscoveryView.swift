f3026712d61f71e06da4b274b035cd31b044ccb7
Nedyalko Tenev <ntenev@outlook.com>
2024-03-04 14:27:34 +0100

    fix of previous commit

--- EggBay/Views/DiscoveryView.swift
+++ EggBay/Views/DiscoveryView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct DiscoveryView: View {
     var body: some View {
-    Text("Discovery")
+        List
+        {
+            LargeBlock(location: "Eindhoven", shopname: "Farm name1", description: "Shop1")
+                .listRowSeparator(.hidden)
+            LargeBlock(location: "Amsterdam", shopname: "Farm name2", description: "Shop2")
+                .listRowSeparator(.hidden)
+            LargeBlock(location: "Tilburg", shopname: "Farm name3", description: "Shop3")
+                .listRowSeparator(.hidden)
+        }
+        .scrollContentBackground(.hidden)
+
+
     }
 }
 
